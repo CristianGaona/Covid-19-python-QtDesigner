ac02565346b2021c55f0fd60476345e54235da4f
CristianGaona <crgaonas24@gmail.com>
2021-01-21 17:57:00 -0500

    Agregando gráficas de Estados por casos y muertes

--- src/main.py
+++ src/main.py
@@ -13,32 +13,37 @@ import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 #Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+
 class MatplotlibWidget(QMainWindow):
 
     def __init__(self):
         QMainWindow.__init__(self)
 
         loadUi("appMain.ui", self)
-
+        self.getCSV()
+        self.indexState()
         self.setWindowTitle("PyQt5 & Matplotlib Example GUI")
-
-       # self.bntCasos.clicked.connect(self.plotPaisCasos)
-       # self.bntMuertes.clicked.connect(self.plotPaisMuertes)
-        self.bntBoth.clicked.connect(self.plotAmbos)
-
-        self.bntCargarCSV.clicked.connect(self.getCSV)
-
         self.addToolBar(NavigationToolbar(self.MplWidget.canvas, self))
+        self.comboBoxCountry.activated.connect(self.clearStates)
+        self.comboBoxCountry.activated.connect(self.loadEstados)
+        self.radioButtonCasos.clicked.connect(self.btnstate)
+        self.radioButtonMuertes.clicked.connect(self.btnstate)
 
-        if self.radioButtonCasos.isCheckable():
-            self.radioButtonCasos.clicked.connect(self.plotPaisCasos)
-        if self.radioButtonMuertes.isCheckable():
-            self.radioButtonMuertes.clicked.connect(self.plotPaisMuertes)
-        if self.radioButtonAmbos.isCheckable():
-            self.radioButtonAmbos.clicked.connect(self.plotAmbos)
+    # =============== Métodos ======================
+    # Definir estos de los radioButton
+    def btnstate(self):
+        #self.indexState()
 
+        if self.radioButtonCasos.isChecked():
+            self.comboBoxCountry.activated.connect(self.plotPaisCasos)
+            self.comboBoxEstado.activated.connect(self.plotEstadosCasos)
 
+        elif self.radioButtonMuertes.isChecked():
+            self.comboBoxCountry.activated.connect(self.plotPaisMuertes)
+            self.comboBoxEstado.activated.connect(self.plotEstadoMuertes)
 
+    # Casos pais
     def plotPaisCasos(self):
         self.MplWidget.canvas.axes.cla()
         pais = self.comboBoxCountry.currentText()
@@ -51,8 +56,39 @@ class MatplotlibWidget(QMainWindow):
         self.MplWidget.canvas.axes.set_ylabel("Cantidad de Casos ")  # Inserta el título del eje Y
         self.MplWidget.canvas.draw()
 
-    def plotPaisMuertes(self):
+    # Casos estado
+    def plotEstadosCasos(self):
+        self.MplWidget.canvas.axes.cla()
+        pais = self.comboBoxEstado.currentText()
+        r = self.indState.loc[[pais], ['Date', 'Casos']]
+        x = r.iloc[:, 0]
+        y = r.iloc[:, 1]
+        self.MplWidget.canvas.axes.plot(x, y)  # Dibuja el gráfico
+        self.MplWidget.canvas.axes.set_title("Casos Covid pais de " + pais)
+        self.MplWidget.canvas.axes.set_xlabel("Fecha")  # Inserta el título del eje X
+        self.MplWidget.canvas.axes.set_ylabel("Cantidad de Casos ")  # Inserta el título del eje Y
+        self.MplWidget.canvas.draw()
 
+    # Muertes estado
+    def plotEstadoMuertes(self):
+        self.aux = 4
+        print(self.aux)
+        self.MplWidget.canvas.axes.cla()
+        pais = self.comboBoxEstado.currentText()
+        r = self.indState.loc[[pais], ['Date', 'Muertes']]
+        x = r.iloc[:, 0]
+        y = r.iloc[:, 1]
+        self.MplWidget.canvas.axes.bar(x, y)  # Dibuja el gráfico
+        self.MplWidget.canvas.axes.set_title("Muertes Covid pais de " + pais)
+        self.MplWidget.canvas.axes.set_xlabel("Fecha")  # Inserta el título del eje X
+        self.MplWidget.canvas.axes.set_ylabel("Cantidad de muertes")  # Inserta el título del eje Y
+        self.MplWidget.canvas.draw()
+
+    # Muertes pais
+    def plotPaisMuertes(self):
+        self.aux = 2
+        print(self.aux)
+        #self.activateGrpah = 2
         self.MplWidget.canvas.axes.cla()
         pais = self.comboBoxCountry.currentText()
         r = self.dataReady.loc[[pais], ['Date', 'Muertes']]
@@ -63,60 +99,71 @@ class MatplotlibWidget(QMainWindow):
         self.MplWidget.canvas.axes.set_title("Muertes Covid pais de " + pais)
         self.MplWidget.canvas.axes.set_xlabel("Fecha")  # Inserta el título del eje X
         self.MplWidget.canvas.axes.set_ylabel("Cantidad de muertes")  # Inserta el título del eje Y
-
         self.MplWidget.canvas.draw()
 
-
     def plotAmbos(self):
+        pass
+
+    # Método para limpiar el combo box de estados
+    def clearStates(self):
+        self.comboBoxEstado.clear()
+
+    # Método para cargar los estados en combo box
+    def loadEstados(self):
+        listEstado = []
+        listEstado.append('All')
         pais_data = self.comboBoxCountry.currentText()
-        print(pais_data)
         estate = self.data_r[self.data_r['Country'] == pais_data].groupby('State')
-        self.comboBoxEstado.addItems(list(estate.groups.keys()))
-        print(estate.groups.keys())
 
-        #self.comboBoxEstado.clear() #Borrar variable de combo
+        # Colocal la palabra ALL al inicio del combo box
+        for i in estate.groups.keys():
+            if i != 'All':
+                listEstado.append(i)
 
-        # Esta función abre el archivo CSV
+
+        self.comboBoxEstado.addItems(list(listEstado))
+
+    # Esta función abre el archivo CSV
     def getCSV(self):
-        filePath, _ = QtWidgets.QFileDialog.getOpenFileName(self, 'Open file', '/Users/crgao')
-        if filePath != "":
-            print("Dirección", filePath)  # Opcional imprimir la dirección del archivo
-            self.data_fr = pd.read_csv(str(filePath), sep=';', na_values="All")
-            self.data_fr = pd.melt(self.data_fr, id_vars=['Country', 'State'], var_name='Date',
-                                   value_name='cases_deaths')
+        data_path = '../data/covid_data.csv'
 
-            # Separar casos y muertes en columnas diferentes
-            separated_df = self.data_fr["cases_deaths"].str.split(expand=True)
-            separated_df.columns = ['Casos', 'Muertes']
-            self.data_fr['Casos'] = separated_df['Casos']
-            self.data_fr['Muertes'] = separated_df['Muertes']
+        self.data_fr = pd.read_csv(data_path, sep=';')
+        self.data_fr = pd.melt(self.data_fr, id_vars=['Country', 'State'], var_name='Date',
+                               value_name='cases_deaths')
 
+        # Separar casos y muertes en columnas diferentes
+        separated_df = self.data_fr["cases_deaths"].str.split(expand=True)
+        separated_df.columns = ['Casos', 'Muertes']
+        self.data_fr['Casos'] = separated_df['Casos']
+        self.data_fr['Muertes'] = separated_df['Muertes']
 
-            # Eliminar columna cases_deaths
-            self.dataReady = self.data_fr.drop(['cases_deaths'], axis=1)
+        # Eliminar columna cases_deaths
+        self.dataReady = self.data_fr.drop(['cases_deaths'], axis=1)
 
-            # Conversión de Object a diferentes tipos
-            self.dataReady['Casos'] = self.dataReady['Casos'].astype('int')
-            self.dataReady['Muertes'] = self.dataReady['Muertes'].astype('int')
-            self.dataReady['Date'] = pd.to_datetime(self.dataReady['Date'])
-            self.dataReady['State'] = self.dataReady['State'].astype('str')
+        # Conversión de Object a diferentes tipos
+        self.dataReady['Casos'] = self.dataReady['Casos'].astype('int')
+        self.dataReady['Muertes'] = self.dataReady['Muertes'].astype('int')
+        self.dataReady['Date'] = pd.to_datetime(self.dataReady['Date'])
+        self.dataReady['State'] = self.dataReady['State'].astype('str')
 
-            # Reemplazar los nan por All
-            self.data_r = self.dataReady.replace("nan", "All")
+        # Reemplazar los nan por All
+        self.data_r = self.dataReady.replace("nan", "All")
 
-            #Agrupar Paises
-            self.grouped = self.data_r.groupby(['Country'])
-            #df2 = grouped.sum()
+        # Agrupar Paises
+        self.grouped = self.data_r.groupby(['Country'])
 
-            #Agregar Items a ComboBox Paises
-            self.comboBoxCountry.addItems(list(self.grouped.groups.keys()))
+        # Agregar Items a ComboBox Paises
+        self.comboBoxCountry.addItems(list(self.grouped.groups.keys()))
 
-            # Agregar indice
-            self.dataReady.set_index("Country", inplace=True)
+        # Agregar indice
+        self.dataReady.set_index("Country", inplace=True)
 
+        self.loadEstados()
 
-
-
+    # Asignar indice para graficas de estado
+    def indexState(self):
+        self.indState = self.data_r
+        self.indState.set_index("State", inplace=True)
 
 app = QApplication([])
 window = MatplotlibWidget()
